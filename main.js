3770962e4dd29130f191135dfd9a7f0385f99cbc
Amr Loksha <amrloksha151@proton.me>
2026-07-09 14:04:04 +0300

    done

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import './style.css';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // manipulate later based on the models
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.getElementById('bg')
 });
@@ -13,42 +12,23 @@ const hdriLoader = new RGBELoader()
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const GLBLoader = new GLTFLoader();
-const clock = new THREE.Clock();
-let mixer = [];
+const myPhotoTexture = new THREE.TextureLoader().load('./textures/amrloksha.jpeg');
+const myPhoto = new THREE.Mesh(new THREE.SphereGeometry(3, 30, 30), new THREE.MeshStandardMaterial({ map: myPhotoTexture }));
+scene.add(myPhoto);
+myPhoto.position.set(0, 0, -25);
 
-
-GLBLoader.load( '/animated_butterfly.glb', function ( gltf ) {
-    const butterFly = gltf.scene;
-    butterFly.scale.set(0.3, 0.3, 0.3);
-    butterFly.position.set(5, 1, -10);
-    scene.add(butterFly);
-
-    for (let i = 0; i < 10; i++) {
-        const clone = butterFly.clone(true);
-        clone.scale.set(0.3, 0.3, 0.3);
-        clone.position.set(Math.random() * 5, Math.random() * 5, -10 + Math.random() * 5);
-        scene.add(clone);
-        mixer.push(new THREE.AnimationMixer(clone));
-    }
-
-    mixer.push(new THREE.AnimationMixer(butterFly));
-    const animation = gltf.animations[0];
-    for (let i = 0; i < mixer.length; i++) {
-        mixer[i].clipAction(animation).play();
-    }
-})
+const myCubicPhoto = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), new THREE.MeshStandardMaterial({ map: myPhotoTexture }));
+scene.add(myCubicPhoto);
+myCubicPhoto.position.set(5, 0, -25);
 
 function animate() {
-    const delta = clock.getDelta();
-    for (let i = 0; i < mixer.length; i++) {
-        mixer[i].update(delta);
-    }
+    myPhoto.rotation.y += 0.05;
+    myPhoto.rotation.x += 0.01;
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
 
-hdriLoader.load( '/fireplace_2k.hdr', function ( texture ) {
+hdriLoader.load( './textures/fireplace_2k.hdr', function ( texture ) {
   const hdri = pmremGenerator.fromEquirectangular( texture ).texture;
   texture.dispose(); 
   scene.environment = hdri;
@@ -58,9 +38,34 @@ hdriLoader.load( '/fireplace_2k.hdr', function ( texture ) {
 
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top;
-    camera.position.z = t * +0.01;
-    camera.rotation.y = t * +0.001;
+    camera.position.z = t * +0.03;
+    camera.rotation.y = t * +0.002;
+    myPhoto.position.x = t * 0.01;
+    myCubicPhoto.rotation.y += 0.05;
 }
 
 document.body.onscroll = moveCamera;
 moveCamera();
+
+const up = document.getElementById('up');
+const down = document.getElementById('down');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+function movePhoto(event) {
+    if (event.target === up || event.key === 'ArrowUp') {
+        myCubicPhoto.rotation.x +=0.1;
+    } else if (event.target === down || event.key === 'ArrowDown') {
+        myCubicPhoto.rotation.x -= 0.1;
+    } else if (event.target === left || event.key === 'ArrowLeft') {
+        myCubicPhoto.rotation.y -= 0.1;
+    } else if (event.target === right || event.key === 'ArrowRight') {
+        myCubicPhoto.rotation.y += 0.1;
+    }
+}
+
+up.addEventListener('click', movePhoto);
+down.addEventListener('click', movePhoto);
+left.addEventListener('click', movePhoto);
+right.addEventListener('click', movePhoto);
+document.body.addEventListener('keydown', movePhoto);
